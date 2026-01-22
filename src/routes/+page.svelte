83da502fac233fafbcb807281fd370dcb321e864
Endre Márk Borza <endremborza@gmail.com>
2026-01-22 19:39:42 +0100

    repos init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import convert from 'xml-js';
+	import githubRepos from '$lib/data/github-repos.json'; // Import the generated JSON
+
+	const RATIO = (Math.sqrt(5) + 1) / 2;
 
 	let blocks: any[] = [];
 
@@ -10,6 +13,11 @@
 		const result = convert.xml2js(xml, { compact: true });
 		blocks = result.root.blocks.block;
 	});
+
+	// Function to get GitHub repo details
+	function getRepoDetails(repoName: string) {
+		return githubRepos.find((repo) => repo.name.toLowerCase() === repoName.toLowerCase());
+	}
 </script>
 
 <svelte:head>
@@ -24,9 +32,22 @@
 				{#each block.elem as elem}
 					<li>
 						{#if elem._attributes.type === 'emb-gh'}
-							<a href="https://github.com/endremborza/{elem.title._text.toLowerCase().replace(/ /g, '-')}">
-								{elem.title._text}
-							</a>
+							{@const repoDetails = getRepoDetails(elem.title._text.toLowerCase())}
+							{#if repoDetails}
+								<a href={repoDetails.html_url} target="_blank" rel="noopener noreferrer">
+									{repoDetails.name} - {repoDetails.description} (⭐ {repoDetails.stargazers_count})
+								</a>
+							{:else}
+								<a
+									href="https://github.com/endremborza/{elem.title._text
+										.toLowerCase()
+										.replace(/ /g, '-')}"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									{elem.title._text} (Repo details not found)
+								</a>
+							{/if}
 						{:else}
 							{elem.title._text}
 						{/if}
