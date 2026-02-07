06d6530f3f0dcb1cdde255768863531c987fab7f
Endre Márk Borza <endremborza@gmail.com>
2026-02-07 21:20:51 +0100

    simplify root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
 	import FractalGrid from '$lib/components/FractalGrid.svelte';
-	import fractalData from '$lib/data/fractal-data.json';
-	import githubRepos from '$lib/data/github-repos.json';
-	import type { TileContent, TileContents } from '$lib/util';
-
-	let initTiles = [
-		{ title: 'T1', body: '<span>Tile one</span>', id: 't0' },
-		{ title: 'T2', body: '<span>Tile two with longer text</span>', id: 't1' },
-		{ title: 'T3', body: '<span>Tile three</span>', id: 't2' },
-		{ title: 'T4', body: '<span>Tile four</span>', id: 't3', children: ['t4', 't0'] },
-		{ title: 'T5', body: '<span>Tile five</span>', id: 't4', children: ['t2', 't1', 't3'] }
-	];
-
-	let tileContents: TileContents = Object.fromEntries(initTiles.map((e) => [e.id, e]));
+	import treeData from '$lib/data/tree-data.json';
+	import type { TileContents } from '$lib/util';
+	let tileContents: TileContents = Object.fromEntries(treeData.map((e) => [e.id, e]));
 </script>
 
 <svelte:head>
@@ -20,7 +10,7 @@
 </svelte:head>
 
 <div>
-	<FractalGrid {tileContents} />
+	<FractalGrid {tileContents} rootId={'home'} />
 </div>
 
 <style>
