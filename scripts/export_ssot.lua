a6c04c37386a2c5ac23d18259b7f04896f736b18
Endre Márk Borza <endremborza@gmail.com>
2026-02-28 12:00:47 +0100

    clean and move to logos

--- scripts/export_ssot.lua
+++
@@ -1,6 +0,0 @@
-local script_dir = arg[0]:match("(.*/)")
-package.path = script_dir .. "?.lua;" .. script_dir .. "../data/ssot/?.lua;" .. package.path
-
-local encode = require("json_encode")
-local data = require("init")
-print(encode(data))
